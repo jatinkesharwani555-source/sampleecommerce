fbc8daa11d158a61272045d74e07ddf0c04a56eb
Jatin Kesharwani <krishnakesharwani555@gmail.com>
2026-03-12 19:27:43 +0530

    last change

--- src/components/Admin/AllUsers.jsx
+++ src/components/Admin/AllUsers.jsx
@@ -13,8 +13,7 @@ const AllUsers = () => {
     try {
       setLoading(true);
       const response = await getAllUsersApi();
-      console.log(response.data.data);
-      setAllUsers(response.data.data.allUsers);
+      setAllUsers(response.data.data);
     } catch (err) {
       setServerError(err.response?.data?.message || "SOMETHING WENT WRONG");
     } finally {
