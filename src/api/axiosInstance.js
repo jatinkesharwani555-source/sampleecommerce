bfc52bad2684f082557440963a0a3554b532a511
Jatin Kesharwani <krishnakesharwani555@gmail.com>
2026-03-18 22:56:05 +0530

    change

--- src/api/axiosInstance.js
+++ src/api/axiosInstance.js
@@ -1,6 +1,7 @@
 import axios from "axios";
 
 const axiosInstance = axios.create({
+  // baseURL: "http://localhost:3000/api",
   baseURL: "https://api.kesharwanimart.in/api",
   withCredentials: true,
 });
